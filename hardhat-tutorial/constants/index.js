c8c7a670a23b91beac6e4cde9faa64cb4eb479ea
umermjd11 <umermjd11@gmail.com>
2022-03-13 06:22:17 +0900

    learnweb3ico initiated

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xb40b9854A7ab78274aE9fcEfcf9a49a6e12c8377";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
